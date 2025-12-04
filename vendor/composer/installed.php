8467a2ae0f457dda16f3c586ac7465e93f534d05
CSfrogy <lonelyguse@gmail.com>
2025-12-04 12:41:41 +0800

    composer package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'php/yourproject',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eb239f9f669b2905d53b3ca18276c9a74df6745e',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'php/yourproject' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eb239f9f669b2905d53b3ca18276c9a74df6745e',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
